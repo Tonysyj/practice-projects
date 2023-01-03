6fa5ab505f3eb128ef4c690eea7f068904a9b8bf
Tonysyj <sunyuejun8623@gmail.com>
2023-01-03 13:52:32 +1000

    projects added

---
+++ Project 2_ProgressBar-bug待修改/script.js
@@ -0,0 +1,81 @@
+const progressContainer = document.querySelector(".progressContainer")
+const progress = document.getElementById("progress")
+const prev = document.getElementById("prev")
+const next = document.getElementById("next")
+
+
+// set progressStep prompt & add into HTML
+const progressStep = prompt('Please Enter the Amount of Steps:')
+for(let i = 0; i < progressStep; i++) {
+  let newDiv = document.createElement('div')
+  newDiv.setAttribute('class', 'step')
+  newDiv.textContent = i + 1
+  progressContainer.appendChild(newDiv)
+}
+const step = document.querySelectorAll('.step')
+step[0].classList.add('active')
+
+
+// active step with btn
+let currentActive = 1
+
+next.addEventListener('click', () => {
+  currentActive ++
+  if (currentActive > step.length) {
+    currentActive = step.length
+  }
+  update()
+})
+
+prev.addEventListener('click', () => {
+  currentActive --
+  if (currentActive < 1) {
+    currentActive = 1
+  }
+  update()
+})
+
+
+// active step with click
+step.forEach(
+  (step, index) => {
+    step.addEventListener('click', () => {
+      step.classList.toggle('active')
+      currentActive = index + 1
+      update()
+      console.log(currentActive)
+    })
+  })
+
+
+function update() {
+  step.forEach(
+    (step, idx) => {
+      if (idx < currentActive) {
+        step.classList.add('active')
+      }
+      else {
+        step.classList.remove('active')
+      }
+    }
+  )
+  
+
+  // active progress line
+  const active = document.querySelectorAll('.active')
+  progress.style.width = (active.length - 1) / (step.length - 1) * 100 + '%'
+
+
+  // btn disabled
+  if (currentActive === 1) {
+    prev.disabled = true
+  }
+  else if (currentActive === step.length) {
+    next.disabled = true
+  }
+  else {
+    prev.disabled = false
+    next.disabled = false
+}
+}
+
